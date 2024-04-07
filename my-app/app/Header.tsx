0a15fc94cee992bc6370c7f87daec5f6277e1ec0
marharyta <marharyta.obraztsova@gmail.com>
2024-04-07 14:30:07 +0300

    initial commit

---
+++ my-app/app/Header.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { ModeToggle } from "@/lib/theme-toggle";
+
+export default function Header() {
+  return <ModeToggle />;
+}
---
+++ my-app/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <main className="flex flex-col items-center justify-between p-24"></main>
+    </div>
+  );
+}
